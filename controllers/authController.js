2a421fb5bd400b47bde7304a31602663eb63adda
jbc2313 <jamesbcorbin@gmail.com>
2022-09-10 08:34:45 -0500

    added test route for user login

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,11 +6,13 @@ const signup = (req, res) => {
 }
 
 const login = (req, res) => {
-
+  console.log('logout route pinged')
+  res.json({ id: 1, name: 'J Carson', email: 'jcarson@yahoo.com' })
 }
 
 const logout = (req, res) => {
   console.log('logout route pinged')
+  console.log('this is the user req' + req.body)
   res.json({msg: 'This is a test'})
 }
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,4 +6,8 @@ const authCtrl = require('../controllers/authController');
 
 router.get('/logout', authCtrl.logout);
 
+router.post('/login', authCtrl.login)
+
+
+
 module.exports = router
